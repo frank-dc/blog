2f11d10ba57d08c85bf15a13776ccaff93926615
frank-dc <frank525dc@gmail.com>
2023-07-12 21:18:11 +0800

    包：其实就是文件夹。 作用：对类进行分类管理。
    包的划分：
        举例：
            学生的增加、删除、查询、修改
            老师的增加、删除、查询、修改
            工人的增加、删除、查询、修改
        1: 按照功能划分
            cn.itcast.add
                AddStudent
                AddTeacher
                AddWorker
            cn.itcast.delete
                DeleteStudent
                DeleteTeacher
                DeleteWorker
            cn.itcast.find
            cn.itcast.update
        2: 按照模块划分
            cn.itcast.student
                Add
                Delete
                Update
                Find
            cn.itcast.teacher
                Add
                Delete
                Update
                Find
            cn.itcast.worker
                Add
                Delete
                Update
                Find
    
    定义包的格式：
        package 包名;
    
    带包的编译和运行：
        手动式：
            A:编译带包的类文件
            B:自己手动创建包
            C:把编译生成的class文件放到包里面去
            D:运行即可，注意要带包名
                java cn.itcast.HelloWorld
        自动式：
            A:编译的时候带一个参数 -d
                javac -d . HelloWorld.java
            B:运行即可，注意要带包名
                java cn.itcast.HelloWorld
    
    注意事项：
        1. package语句必须是程序的第一条可执行的代码。
        2. package语句在一个Java文件中只能有一个。
        3. 如果没有package，默认表示无包名。

---
+++ program/java/learnjava/src/main/java/org/frank/reference/thinking/package_keyword/package_one/Demo.java
@@ -0,0 +1,7 @@
+package org.frank.reference.thinking.package_keyword.package_one;
+
+public class Demo {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ program/java/learnjava/src/main/java/org/frank/reference/thinking/package_keyword/package_two/Test.java
@@ -0,0 +1,11 @@
+package org.frank.reference.thinking.package_keyword.package_two;
+
+import org.frank.reference.thinking.package_keyword.package_one.Demo;
+
+public class Test {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        int res = d.sum(10, 20);
+        System.out.println(res);
+    }
+}
